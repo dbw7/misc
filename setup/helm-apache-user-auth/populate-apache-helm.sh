0df5bc87b565fd3c721c3245948b96d720c8caa4
dbw7 <danial.bekhit@suse.com>
2024-03-01 02:57:28 +0000

    update populate

--- setup/helm-apache-user-auth/populate-apache-helm.sh
+++ setup/helm-apache-user-auth/populate-apache-helm.sh
@@ -5,15 +5,15 @@ set -o pipefail
 
 cd /var/www/html/helm-charts/
 
-helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
-helm pull rancher-latest/rancher
+sudo helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+sudo helm pull rancher-latest/rancher
 
 
-helm pull oci://registry-1.docker.io/bitnamicharts/apache
+sudo helm pull oci://registry-1.docker.io/bitnamicharts/apache
 
 echo -n "Please type in the ip of the host: "
 read ip_address
 
-helm repo index . --url "http://$ip_address:8092/"
+sudo helm repo index . --url "http://$ip_address:8092/"
 
 echo "Complete\n Try to do curl -u \"user:root\" http://$ip_address:8092/index.yaml"
